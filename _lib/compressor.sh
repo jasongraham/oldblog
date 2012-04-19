0c1b004649343bf5dd0b3758ea3ec8ffd9309990
Jason Graham <jason@the-graham.com>
2012-04-18 21:12:11 -0700

    Compress images as well

--- _lib/compressor.sh
+++ _lib/compressor.sh
@@ -14,3 +14,14 @@ do
     gzip --best -n <$file >$file.gz
 done
 
+# compress jpegs
+find _site -type f | egrep "*.jpg" | while read file
+do
+    jpegtran -copy none -optimize -progressive -outfile $file.crushed $file
+    find _site -name "*.crushed" -print | sed "s/\\(.*\\)\\.jpg.crushed/ & \\1.jpg/" | xargs -L1 mv
+done
+
+
+# compress pngs
+find _site -name "*.png" -print | xargs -L1 pngcrush -q -e .crushed -rem time -rem alla -rem allb -l 9 -w 32 -plte_len 0 -reduce -rem gAMA -rem cHRM -rem iCCP -rem sRGB -brute
+find _site -name "*.crushed" -print | sed "s/\\(.*\\)\\.crushed/ & \\1.png/" | xargs -L1 mv
